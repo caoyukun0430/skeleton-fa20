b31614110748fd10d29889f066e04a1e216c8a79
Yukun Cao <caoyukunk9999@126.com>
2021-04-15 00:12:34 +0200

    comments added for KthIntList

--- dis5/src/KthIntList.java
+++ dis5/src/KthIntList.java
@@ -24,11 +24,21 @@ public class KthIntList implements Iterator<Integer> {
             throw new NoSuchElementException();
         }
         Integer nextItem = curList.first;
-        // here we cover both even and odd cases
-        if (curList.rest == null || curList.rest.rest == null) {
-            hasNext = false;
-        } else {
-            curList = curList.rest.rest;
+        // here we cover both even and odd cases,
+        // only when we has next two elements, we continue moving pointer.
+        // Not good, is fixed by 2
+//        if (curList.rest == null || curList.rest.rest == null) {
+//            hasNext = false;
+//        } else {
+//            curList = curList.rest.rest;
+//        }
+        // Since we have k, get inspired by solution
+        for (int i = 0; i < k; i++) {
+            curList = curList.rest;
+            if (curList == null) {
+                hasNext = false;
+                return nextItem;
+            }
         }
         return nextItem;
     }
@@ -37,7 +47,7 @@ public class KthIntList implements Iterator<Integer> {
         IntList L = new IntList(1, null);
         L = new IntList(2, L);
         L = new IntList(3, L);
-        L = new IntList(4, L);
+//        L = new IntList(4, L);
         Iterator<Integer> p = new KthIntList(L, 2);
         while (p.hasNext()) {
             System.out.println(p.next());
